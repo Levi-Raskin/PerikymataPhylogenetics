5b93ad1c0569ba9b34ff13a85594a69fe79abd7b
Levi Yoder Raskin <122696329+Levi-Raskin@users.noreply.github.com>
2026-04-23 17:09:00 -0700

    minor update

--- scripts/fullAnalysis.sh
+++ scripts/fullAnalysis.sh
@@ -2,7 +2,7 @@
 
 ### posterior estimation ###
 # Lower canine with hominins
-/BURL/build/burl \
+BURL/build/burl \
     -i "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/LCdec3_10.csv" \
     -it "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/tree.txt" \
     -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/withGibbs/lc/lc_dec3_10.tsv" \
@@ -14,7 +14,7 @@
     -log F
 
 # Lower canine without hominins
-/BURL/build/burl \
+BURL/build/burl \
     -i "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/LCdec3_10_no_hominin.csv" \
     -it "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/tree.txt" \
     -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/withGibbs/lc/lc_dec3_10_no_hominin.tsv" \
@@ -26,7 +26,7 @@
     -log F
 
 # Lower canine species means
-/BURL/build/burl \
+BURL/build/burl \
     -i "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/LCdec3_10_species_means.csv" \
     -it "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/tree.txt" \
     -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/withGibbs/lc/lc_dec3_10_species_means.tsv" \
@@ -39,7 +39,7 @@
 
 
 # Upper second incisor
-/BURL/build/burl \
+BURL/build/burl \
     -i "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/UI2dec3_10_no_pongo.csv" \
     -it "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/tree.txt" \
     -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/withGibbs/ui2/ui2_dec3_10_no_pongo.tsv" \
