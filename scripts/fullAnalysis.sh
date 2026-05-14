148a4907756cf3c756c636a16c803d6b0d81ea7a
Levi Yoder Raskin <122696329+Levi-Raskin@users.noreply.github.com>
2026-05-14 10:58:05 -0700

    cmits

--- scripts/fullAnalysis.sh
+++ scripts/fullAnalysis.sh
@@ -54,23 +54,23 @@ BURL/build/burl \
     -nt 10 \
     -log F
 
-# ui2 species means
+# Upper second incisor
 BURL/build/burl \
-    -i "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/UI2dec3_10_no_pongo_species_means.csv" \
+    -i "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/UI2dec3_10_no_pongo.csv" \
     -it "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/tree.txt" \
-    -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/withGibbs_v2/ui2/ui2_dec3_10_species_means.tsv" \
+    -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/withGibbs_v2/ui2/ui2_dec3_10_no_pongo.tsv" \
     -n "$nc" \
     -p 1000 \
     -s 1000 \
     -c 10 \
     -nt 10 \
     -log F
-
-# Upper second incisor
+    
+# ui2 species means
 BURL/build/burl \
-    -i "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/UI2dec3_10_no_pongo.csv" \
+    -i "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/UI2dec3_10_no_pongo_species_means.csv" \
     -it "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/data/tree.txt" \
-    -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/withGibbs_v2/ui2/ui2_dec3_10_no_pongo.tsv" \
+    -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/withGibbs_v2/ui2/ui2_dec3_10_species_means.tsv" \
     -n "$nc" \
     -p 1000 \
     -s 1000 \
