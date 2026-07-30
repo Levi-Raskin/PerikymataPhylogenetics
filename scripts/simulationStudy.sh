c61859a15f7ab8e9eb67846ca10f793fc8aa35a3
Levi Yoder Raskin <122696329+Levi-Raskin@users.noreply.github.com>
2026-07-30 07:44:40 -0700

    Update simulationStudy.sh

--- scripts/simulationStudy.sh
+++ scripts/simulationStudy.sh
@@ -3,14 +3,14 @@
 ntips=(8 16 32)
 ntraits=(8 16 32)
 nimp=(0 8 16)
-nobsv=(4 8 16)
+nobsv=(2 4 8 16)
 
-max_jobs=4
+max_jobs=3
 
 run_burlc () {
     local t="$1" tr="$2" im="$3" no="$4"
     BURL_coverage/BURL_coverage/build/burlc \
-        -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/simulation_study/simulated_${t}_tips_${tr}_nimp_${im}_nobs_${no}_traits_20_reps" \
+        -o "/Users/levir/Documents/GitHub/PerikymataPhylogenetics/results/simulation_study/simulated_${t}_tips_${tr}_nimp_${im}_nobs_${no}_traits_100_reps" \
         -c 1000000 \
         -nreps 100 \
         -ntips "$t" \
